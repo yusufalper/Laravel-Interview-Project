de4345fc6af148fa3aa08cc42e91a5d0c264b606
Alper Sari <yusufalpersari@yandex.com>
2020-12-04 15:47:01 +0300

    Company model-migration-controller added

---
+++ database/seeders/CompanySeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CompanySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('companies')->insert([
+            'name' => Str::random(10),
+            'internet_adress' => Str::random(15),
+        ]);
+    }
+}
